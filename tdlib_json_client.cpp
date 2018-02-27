89f56238dd6abb668bd83b240f16c409b21cf777
yaroslavche <yaroslav429@gmail.com>
2018-02-27 14:23:35 +0200

    0.0.3

--- tdlib_json_client.cpp
+++ tdlib_json_client.cpp
@@ -1,4 +1,5 @@
 #include "tdlib_json_client.hpp"
+#include <td/telegram/Log.h>
 
 void TDLibJsonClient::__construct()
 {
@@ -9,30 +10,44 @@ void TDLibJsonClient::__construct()
 
 Php::Value TDLibJsonClient::create()
 {
+    td::Log::set_verbosity_level(1);
     _client = td_json_client_create();
     return _client;
 }
 
 void TDLibJsonClient::destroy()
 {
+    if(_client == NULL) Php::error << "TDLibJsonClient not created. Create client before using" << std::flush;
     td_json_client_destroy(_client);
     return;
 }
 
 Php::Value TDLibJsonClient::execute(Php::Parameters &params)
 {
+    if(_client == NULL) Php::error << "TDLibJsonClient not created. Create client before using" << std::flush;
     td_json_client_execute(_client, params[0]);
     return true;
 }
 
-void TDLibJsonClient::send(Php::Parameters &params)
+Php::Value TDLibJsonClient::send(Php::Parameters &params)
 {
-    td_json_client_send(_client, params[0]);
-    return;
+    if(_client == NULL) Php::error << "TDLibJsonClient not created. Create client before using" << std::flush;
+    const char *query = params[0];
+    double timeout = params[1];
+    Php::out << "send query " << query << " with timeout " << timeout << std::endl << std::flush;
+    td_json_client_send(_client, query);
+    std::string result = "";
+    while(true)
+    {
+        result = td_json_client_receive(_client, timeout);
+        if(!result.empty()) break;
+    }
+    return result;
 }
 
 Php::Value TDLibJsonClient::receive(Php::Parameters &params)
 {
+    if(_client == NULL) Php::error << "TDLibJsonClient not created. Create client before using" << std::flush;
     std::string receive = td_json_client_receive(_client, params[0]);
     return receive;
 }
