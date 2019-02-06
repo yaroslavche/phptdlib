18d6eaed6b32b6814001a68f199ba2aa08d48c43
yaroslavche <yaroslav429@gmail.com>
2019-02-06 13:39:54 +0200

    Update documentation and README. Closes #28
    Minor changes in example.

--- README.md
+++ README.md
@@ -22,42 +22,49 @@ while(true)
 Error_Reporting(E_ALL);
 ini_set('display_errors', 1);
 
-TDApi\LogConfiguration::setLogVerbosityLevel(\TDApi\LogConfiguration::LVL_ERROR);
+$api_id = 11111; // must be integer
+$api_hash = 'abcdef1234567890abcdef1234567890';
+$phone_number = '+380991234567';
 
-$client = new TDLib\JsonClient();
-
-$tdlibParams = new TDApi\TDLibParameters();
-$tdlibParams
-    ->setParameter(\TDApi\TDLibParameters::USE_TEST_DC, true)
-    ->setParameter(\TDApi\TDLibParameters::DATABASE_DIRECOTRY, '/var/tmp/tdlib')
-    ->setParameter(\TDApi\TDLibParameters::FILES_DIRECTORY, '/var/tmp/tdlib')
-    ->setParameter(\TDApi\TDLibParameters::USE_FILE_DATABASE, false)
-    ->setParameter(\TDApi\TDLibParameters::USE_CHAT_INFO_DATABASE, false)
-    ->setParameter(\TDApi\TDLibParameters::USE_MESSAGE_DATABASE, false)
-    ->setParameter(\TDApi\TDLibParameters::USE_SECRET_CHATS, false)
-    // SET API_CODE AND API_HASH
-    ->setParameter(\TDApi\TDLibParameters::API_ID, 123)
-    ->setParameter(\TDApi\TDLibParameters::API_HASH, 'xxx')
-    ->setParameter(\TDApi\TDLibParameters::SYSTEM_LANGUAGE_CODE, 'en')
-    ->setParameter(\TDApi\TDLibParameters::DEVICE_MODEL, php_uname('s'))
-    ->setParameter(\TDApi\TDLibParameters::SYSTEM_VERSION, php_uname('v'))
-    ->setParameter(\TDApi\TDLibParameters::APPLICATION_VERSION, '0.0.8')
-    ->setParameter(\TDApi\TDLibParameters::ENABLE_STORAGE_OPTIMIZER, true)
-    ->setParameter(\TDApi\TDLibParameters::IGNORE_FILE_NAMES, false);
-$result = $client->setTdlibParameters($tdlibParams);
-$result = $client->setDatabaseEncryptionKey();
-// SET PHONE_NUMBER
-$result = $client->setAuthenticationPhoneNumber("xxx");
-// UNCOMMENT WHEN RECEIVE SMS AND INSERT CODE.
-// $result = $client->query(json_encode(['@type' => 'checkAuthenticationCode', 'code' => 'xxx', 'first_name' => 'dummy', 'last_name' => 'dummy']), 10);
-$result = $client->getAuthorizationState();
-$result = $client->query(json_encode(['@type' => 'searchPublicChat', 'username' => 'telegram']), 10);
-var_dump($result);
-
-$allNotifications = $client->getReceivedResponses();
-var_dump($allNotifications);
-
-$client->destroy();
+try {
+    TDApi\LogConfiguration::setLogVerbosityLevel(\TDApi\LogConfiguration::LVL_ERROR);
+    
+    $client = new TDLib\JsonClient();
+    $client->getAuthorizationState();
+    
+    $tdlibParams = new TDApi\TDLibParameters();
+    $tdlibParams
+        ->setParameter(TDApi\TDLibParameters::USE_TEST_DC, true)
+        ->setParameter(TDApi\TDLibParameters::DATABASE_DIRECOTRY, '/var/tmp/tdlib')
+        ->setParameter(TDApi\TDLibParameters::FILES_DIRECTORY, '/var/tmp/tdlib')
+        ->setParameter(TDApi\TDLibParameters::USE_FILE_DATABASE, false)
+        ->setParameter(TDApi\TDLibParameters::USE_CHAT_INFO_DATABASE, false)
+        ->setParameter(TDApi\TDLibParameters::USE_MESSAGE_DATABASE, false)
+        ->setParameter(TDApi\TDLibParameters::USE_SECRET_CHATS, false)
+        ->setParameter(TDApi\TDLibParameters::API_ID, $api_id)
+        ->setParameter(TDApi\TDLibParameters::API_HASH, $api_hash)
+        ->setParameter(TDApi\TDLibParameters::SYSTEM_LANGUAGE_CODE, 'en')
+        ->setParameter(TDApi\TDLibParameters::DEVICE_MODEL, php_uname('s'))
+        ->setParameter(TDApi\TDLibParameters::SYSTEM_VERSION, php_uname('v'))
+        ->setParameter(TDApi\TDLibParameters::APPLICATION_VERSION, '0.0.8')
+        ->setParameter(TDApi\TDLibParameters::ENABLE_STORAGE_OPTIMIZER, true)
+        ->setParameter(TDApi\TDLibParameters::IGNORE_FILE_NAMES, false);
+    $result = $client->setTdlibParameters($tdlibParams);
+    $result = $client->setDatabaseEncryptionKey(); // checkDatabaseEncryptionKey(key) or DESTROY
+    
+    $result = $client->setAuthenticationPhoneNumber($phone_number, 3); // wait response 3 seconds. default - 1.
+    // UNCOMMENT NEXT and COMMENT PREVIOUS LINES WHEN RECEIVE SMS. Set your data
+    // $result = $client->query(json_encode(['@type' => 'checkAuthenticationCode', 'code' => 'xxxxx', 'first_name' => 'dummy', 'last_name' => 'dummy']), 10);
+    
+    $result = $client->getAuthorizationState();
+    $result = $client->query(json_encode(['@type' => 'searchPublicChat', 'username' => 'telegram']), 10);
+    var_dump($result);
+    
+    $allNotifications = $client->getReceivedResponses();
+    var_dump($allNotifications);
+} catch (\Exception $exception) {
+    echo sprintf('something goes wrong: %s', $exception->getMessage());
+}
 ```
 
 ## Required
--- php_examples/client.php
+++ php_examples/client.php
@@ -3,36 +3,48 @@
 Error_Reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if(file_exists(__DIR__ . '/credentials.php')) require_once __DIR__ . '/credentials.php';
-else die('cp php_examples/credentials.php.dist php_examples/credentials.php && edit' . PHP_EOL);
+if (file_exists(__DIR__ . '/credentials.php')) {
+    require_once __DIR__ . '/credentials.php';
+} else {
+    die('cp php_examples/credentials.php.dist php_examples/credentials.php && edit' . PHP_EOL);
+}
 
-TDApi\LogConfiguration::setLogVerbosityLevel(\TDApi\LogConfiguration::LVL_ERROR);
+try {
+    TDApi\LogConfiguration::setLogVerbosityLevel(\TDApi\LogConfiguration::LVL_ERROR);
 
-$client = new TDLib\JsonClient();
-$client->getAuthorizationState();
+    $client = new TDLib\JsonClient();
+    $client->getAuthorizationState();
 
-$tdlibParams = new TDApi\TDLibParameters();
-$tdlibParams
-    ->setParameter(TDApi\TDLibParameters::USE_TEST_DC, true)
-    ->setParameter(TDApi\TDLibParameters::DATABASE_DIRECOTRY, '/var/tmp/tdlib')
-    ->setParameter(TDApi\TDLibParameters::FILES_DIRECTORY, '/var/tmp/tdlib')
-    ->setParameter(TDApi\TDLibParameters::USE_FILE_DATABASE, false)
-    ->setParameter(TDApi\TDLibParameters::USE_CHAT_INFO_DATABASE, false)
-    ->setParameter(TDApi\TDLibParameters::USE_MESSAGE_DATABASE, false)
-    ->setParameter(TDApi\TDLibParameters::USE_SECRET_CHATS, false)
-    ->setParameter(TDApi\TDLibParameters::API_ID, $api_id)
-    ->setParameter(TDApi\TDLibParameters::API_HASH, $api_hash)
-    ->setParameter(TDApi\TDLibParameters::SYSTEM_LANGUAGE_CODE, 'en')
-    ->setParameter(TDApi\TDLibParameters::DEVICE_MODEL, php_uname('s'))
-    ->setParameter(TDApi\TDLibParameters::SYSTEM_VERSION, php_uname('v'))
-    ->setParameter(TDApi\TDLibParameters::APPLICATION_VERSION, '0.0.7')
-    ->setParameter(TDApi\TDLibParameters::ENABLE_STORAGE_OPTIMIZER, true)
-    ->setParameter(TDApi\TDLibParameters::IGNORE_FILE_NAMES, false);
-$result = $client->setTdlibParameters($tdlibParams);
-$result = $client->setDatabaseEncryptionKey(); // checkDatabaseEncryptionKey(key) or DESTROY
-$result = $client->setAuthenticationPhoneNumber($phone_number);
-// $result = $client->query(json_encode(['@type' => 'checkAuthenticationCode', 'code' => 'xxx', 'first_name' => 'dummy', 'last_name' => 'dummy']), 10);
-$result = $client->getAuthorizationState();
-$result = $client->query(json_encode(['@type' => 'searchPublicChat', 'username' => 'telegram']), 10);
-var_dump($result);
-$client->destroy();
+    $tdlibParams = new TDApi\TDLibParameters();
+    $tdlibParams
+        ->setParameter(TDApi\TDLibParameters::USE_TEST_DC, true)
+        ->setParameter(TDApi\TDLibParameters::DATABASE_DIRECOTRY, '/var/tmp/tdlib')
+        ->setParameter(TDApi\TDLibParameters::FILES_DIRECTORY, '/var/tmp/tdlib')
+        ->setParameter(TDApi\TDLibParameters::USE_FILE_DATABASE, false)
+        ->setParameter(TDApi\TDLibParameters::USE_CHAT_INFO_DATABASE, false)
+        ->setParameter(TDApi\TDLibParameters::USE_MESSAGE_DATABASE, false)
+        ->setParameter(TDApi\TDLibParameters::USE_SECRET_CHATS, false)
+        ->setParameter(TDApi\TDLibParameters::API_ID, $api_id)
+        ->setParameter(TDApi\TDLibParameters::API_HASH, $api_hash)
+        ->setParameter(TDApi\TDLibParameters::SYSTEM_LANGUAGE_CODE, 'en')
+        ->setParameter(TDApi\TDLibParameters::DEVICE_MODEL, php_uname('s'))
+        ->setParameter(TDApi\TDLibParameters::SYSTEM_VERSION, php_uname('v'))
+        ->setParameter(TDApi\TDLibParameters::APPLICATION_VERSION, '0.0.8')
+        ->setParameter(TDApi\TDLibParameters::ENABLE_STORAGE_OPTIMIZER, true)
+        ->setParameter(TDApi\TDLibParameters::IGNORE_FILE_NAMES, false);
+    $result = $client->setTdlibParameters($tdlibParams);
+    $result = $client->setDatabaseEncryptionKey(); // checkDatabaseEncryptionKey(key) or DESTROY
+
+    $result = $client->setAuthenticationPhoneNumber($phone_number, 3); // wait response 3 seconds. default - 1.
+    // UNCOMMENT NEXT and COMMENT PREVIOUS LINES WHEN RECEIVE SMS. Set your data
+    // $result = $client->query(json_encode(['@type' => 'checkAuthenticationCode', 'code' => 'xxxxx', 'first_name' => 'dummy', 'last_name' => 'dummy']), 10);
+
+    $result = $client->getAuthorizationState();
+    $result = $client->query(json_encode(['@type' => 'searchPublicChat', 'username' => 'telegram']), 10);
+    var_dump($result);
+
+    $allNotifications = $client->getReceivedResponses();
+    var_dump($allNotifications);
+} catch (\Exception $exception) {
+    echo sprintf('something goes wrong: %s', $exception->getMessage());
+}
